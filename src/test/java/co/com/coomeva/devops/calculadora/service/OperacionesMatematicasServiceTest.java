4ca90255cbd3f11c8e807b83b1c7b7d7eb178eab
Diego Armando Gomez <diegomezusb@gmail.com>
2021-02-04 08:34:18 -0500

    Se adiciona un stage de clean

--- src/test/java/co/com/coomeva/devops/calculadora/service/OperacionesMatematicasServiceTest.java
+++ src/test/java/co/com/coomeva/devops/calculadora/service/OperacionesMatematicasServiceTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class OperacionesMatematicasServiceTest {
